d645ee8e0d943349c10238ba73467395de450721
yohom <yohombao@gmail.com>
2020-01-10 08:44:37 +0800

    feat: 初始化方法加入

--- lib/src/dart/speech_service.dart
+++ lib/src/dart/speech_service.dart
@@ -1,13 +1,17 @@
 import 'package:xfiat_fluttify/src/android/android.export.g.dart';
+import 'package:xfiat_fluttify/src/ios/IFlySpeechUtility.g.dart';
 
 class SpeechService {
   Future<void> init() async {
     return platform(
       android: (pool) async {
         final context = await android_app_Application.get();
-        com_iflytek_cloud_SpeechUtility.createUtility(context, 'var1');
+        await com_iflytek_cloud_SpeechUtility.createUtility(
+            context, com_iflytek_cloud_SpeechConstant.APPID + '=5de4a580');
       },
-      ios: (pool) async {},
+      ios: (pool) async {
+        await IFlySpeechUtility.createUtility('appid=5de4a580');
+      },
     );
   }
 }
